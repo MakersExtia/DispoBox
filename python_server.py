3eb9b745728eef2d35ac6a68c1e5d0da8da33e23
georgespagis <ghpagis@gmail.com>
2016-08-05 08:23:22 +0200

    Modif en profondeur du code pour plus de fiabilité et de meilleurs réglages des temps de MAJ des box

--- python_server.py
+++ python_server.py
@@ -16,11 +16,27 @@ dbConfig = {'user' : 'root',
             'host' : 'localhost',
             'database' : 'dispobox'}
 
+all_data_table = "stats"
+
+
 # Etat actuel des box (utilité à démontrer)
-current_state = {'00' : '-1',
+current_state = {'0' : '-1',
         '41' : '-1',
         '42' : '-1',
-        '43' : '-1'}
+        '43' : '-1',
+        '44' : '-1',
+        '45' : '-1',
+        '46' : '-1',
+        '47' : '-1',
+        '48' : '-1',
+        '61' : '-1',
+        '62' : '-1',
+        '63' : '-1',
+        '64' : '-1',
+        '65' : '-1',
+        '66' : '-1',
+        '67' : '-1',
+        '68' : '-1'}
 
 
 """
@@ -32,24 +48,30 @@ def active_connexion(myConnexion, myAdress, q):
     LOG.info("Client connecté, adresse IP %s, port %s" % (myAdress[0], myAdress[1]))
     connected = True
     while connected:
-        try:
-            msgClient = myConnexion.recv(1024)
-        except Exception as e:
-            LOG.error("Exception renvoyée lors de la reception des données : "+str(e))
-        if msgClient:
-            LOG.info("Message Recu : "+ msgClient)
-            new_state = create_new_state(msgClient)
-            q.put(new_state)
-            LOG.debug("Nouvel état dans la queue : box "+ new_state[0]+ " état "+new_state[1])
-        else:
-            #Si la connexion s'est arretée, on détruit le process en cours
-            connected = False
+        nbit = 65
+        for i in range(1,nbit+1):
+            if (i<nbit):
+                myConnexion.send("#")
+            else:
+                print("Demande d'infos")
+                myConnexion.send("infos#")
+                try:
+                    msgClient = myConnexion.recv(1024)
+                except Exception as e:
+                    LOG.error("Exception renvoyée lors de la reception des données : "+str(e))
+                if msgClient:
+                    LOG.info("Message Recu : "+ msgClient)
+                    if (msgClient!="rien"):
+                        treat_msg_received(msgClient,q)
+                else:
+                    #Si la connexion s'est arretée, on détruit le process en cours
+                    connected = False
         #except:
         #    raise
         #    LOG.error("CRASH DANS LE PROCESS :"+ multiprocessing.current_process())
         #    connected = False
         #    pass
-        time.sleep(1)
+            time.sleep(0.05)
     LOG.info("Connexion perdue avec %s" %(myAdress[0]))
 
 
@@ -57,7 +79,18 @@ def active_connexion(myConnexion, myAdress, q):
     Cette fonction est appelée par chaque process de connexion dès qu'un message est reçu. 
     Pour chaque message, elle sépare le numero du box et son état et renvoie le couple [nom, état]
 """
-def create_new_state(msgClient):
+def treat_msg_received(msgClient,q):
+    box_nb = ["0","0","0","0"]
+    box_state = ["-1","-1","-1","-1"]
+    nb_box = int(msgClient[0])
+    print("nb de box ayant changé = "+str(nb_box))
+    for i in range(1,nb_box+1):
+        box_nb[i-1] = msgClient[3*i-2] + msgClient[3*i-1]
+        box_state[i-1]= msgClient[3*i]
+        q.put([box_nb[i-1],box_state[i-1]])
+        LOG.debug("Nouvel état dans la queue : box "+ box_nb[i-1]+ " état "+box_state[i-1])
+
+    """
     msg1 = True
     box_nb = ''
     box_state = ''
@@ -69,8 +102,9 @@ def create_new_state(msgClient):
                 box_nb = box_nb + c
             else:
                 box_state = box_state + c
+    q.put([box_nb, box_state])
     return [box_nb, box_state]
-
+    """
 
 """
     Boucle attendant une nouvelle connexion et créant un nouveau process lançant la fonction active_connexion
@@ -109,12 +143,28 @@ def MAJ_current_state(sql_connexion, stop_event, q, cursor):
         except mysql.connector.Error as err:
             LOG.error("Erreur lors de la MAJ de la BDD : ",str(err))
         sql_connexion.commit()
+
+        store_complete_datas(cursor)
         time.sleep(0.5)
     LOG.debug("Sortie du thread de MAJ de la queue")
 
 
 
-
+"""
+    Fonction qui permet de stocker l'état général du système et l'heure à laquelle il a changé
+    
+"""
+def store_complete_datas(cursor):
+    # On cree la requete SDQL ne premiere fois
+    SQL_order = "INSERT INTO "+all_data_table+" (`0`,"             # syntaxe de debut et champ '0'
+    for i in range(41,48)+range(61,67):
+        SQL_order += "`"+str(i)+"`,"                               # champs 41 à 67                        
+    SQL_order += "`68`) VALUES ("+str(current_state['0'])+","      # champs 68, syntax des valeurs et valeur 0
+    for j in range(41,48)+range(61,67):
+        SQL_order = SQL_order + str(current_state[str(j)])+","     # valeurs 41 à 67
+    SQL_order = SQL_order+str(current_state['68'])+");"            # valeur 68
+    cursor.execute(SQL_order)
+    
 
 
 LOG=logger.create_logger()
@@ -142,19 +192,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     sock.bind((HOST,PORT))
 except socket.error, msg:
-    LOG.error("La liaison du socket a échoué : "+ msg)
+    LOG.error("La liaison du socket a échoué : "+ str(msg))
     sys.exit()
     
 
 # Creation de la queue
 state_q = Queue()
 
-
 LOG.info("Serveur prêt")
 print "Appuyez sur n'importe quelle touche pour arreter le programme"
-current_state["00"] = 1
+
+# informe la BDD que le système est en marche
+current_state["0"] = 1
 cursor.execute("UPDATE current_state SET state = 1 WHERE name = 00")
+store_complete_datas(cursor)
 sql_con.commit()
+
 stop_event = threading.Event()
 connex_thread = threading.Thread(target = wait_connexion, args=(stop_event, sock, state_q, ))
 connex_thread.daemon = True
@@ -172,7 +225,10 @@ print("##################################################")
 
 #cursor.execute("UPDATE current_state SET state = -1 WHERE name = 0")
 cursor.execute("UPDATE current_state SET state = -1")
+current_state["0"]= -1
+store_complete_datas(cursor)
 sql_con.commit()
+
 stop_event.set()
 sock.shutdown(2)
 sock.close()
