b0dfea67cda33db75c95ec4074e3c76b514f132a
georgespagis <ghpagis@gmail.com>
2017-01-24 14:29:35 +0100

    Fichier permettant d'envoyer les logs par mail, correction du serveur python pour qu'il fonctionne avec le 6 ème étage

--- Serveur_python/envoi_mail_log.py
+++ Serveur_python/envoi_mail_log.py
@@ -4,13 +4,20 @@
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import Encoders
 
 msg = MIMEMultipart()
 msg['From'] = 'makersextia@gmail.com'
 msg['To'] = 'gpagis@extia.fr'
 msg['Subject'] = 'Erreur dispobox' 
-message = 'Il y a eu une erreur sur le dispobox !'
-msg.attach(MIMEText(message))
+
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open("log_file.log", "rb").read())
+Encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="log_file.txt"')
+
+msg.attach(part)
 mailserver = smtplib.SMTP('smtp.gmail.com', 587)
 mailserver.ehlo()
 mailserver.starttls()
